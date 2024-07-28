1461f74a40a0b7ecc39637502293e7974056de8e
LuisPereira-20 <151740701+LuisPereira-20@users.noreply.github.com>
2024-07-28 00:07:09 -0400

    controller part 1

---
+++ backend/Controller/Controlador_producto.js
@@ -0,0 +1,77 @@
+import producto from "../Model/modelo_producto.js";
+import categoria from "../Model/modelo_categoria.js";
+import regex from "../Tools/validacion.js";
+import opciones from "../Tools/opciones.js";
+
+export const getProductos = async (req, res) => {
+    try {
+    opciones.page = parseInt(req.query.page) || 1;
+    opciones.limit = parseInt(req.query.limit) || 12;
+    const Productos = await Productos.paginate({eliminado : false} , opciones);
+    res.status(200).json(Productos);
+}  catch (error) {
+    res.status(404).json({ error : error.message});
+}
+}
+
+export const getProducto = async (req, res) => {
+    try {
+    const producto = await producto.paginate({id : req.params.id, eliminado : false}, opciones);
+    res.status(200).json(producto);
+}  catch (error) {
+    res.status(404).json({ error : error.message});
+}
+}
+
+export const postProducto = async (req, res) => {
+    try {
+        if (!regex.nombre.test(req.body.nombre)){
+            return res.status(500) .json({ error : "El nombre no es valido" });
+        }
+            
+        if  (!regex.descripcion.test(req.body.descripcion)){
+            return res.status(500) .json({ error : "La descripcio no es valida" });
+        }
+        const categoria_Existente = await categoria.findById(req.body.categoria);
+        if (!categoria_Existente){
+            return res.status(404).json({ error : "La categoria no existe" });
+        }
+        const producto = new producto(req.body);
+        await producto.save();
+        res.status(200).json(producto);} 
+        catch (error) {
+        res.status(404).json({ error : error.message});
+        }
+}
+
+export const editarProducto = async (req, res) => {
+    try{
+        req.body.fechaActualizacion = Date.now();
+        if (!regex.nombre.test(req.body.nombre)){
+            return res.status(500).json({ error : "El nombre no es valido" });
+    }
+    if  (!regex.descripcion.test(req.body.descripcion)){
+        return res.status(500).json({ error : "La descripcio no es valida" });
+    }
+    const categoria_Existente = await categoria.findById(req.body.categoria);
+    if (!categoria_Existente){
+        return res.status(404).json({ error : "La categoria no existe" });
+    }
+    const producto = await Producto.findByIdAndUpdate({ _id : req.params.id, eliminado : false}, req.body, {new : true});
+    const producto_paginate = await producto_paginate.paginate({id : req.params.id, eliminado : false}, opciones);
+    res.status(200).json(producto_paginate);
+    } catch (error) {
+        res.status(404).json({ error : error.message});
+    }
+}
+
+export const eliminarProducto = async (req, res) => {
+    try {
+        const producto = await producto.findByIdAndUpdate({ _id : req.params.id, eliminado : false}, {eliminado : true, fechaEliminacion : Date.now()}, {new : true});
+        const producto_paginate = await Producto.paginate({id : req.params.id, eliminado : false}, opciones);
+        res.status(200).json(producto_paginate);
+    } catch (error) {
+        res.status(404).json({ error : error.message});
+    }
+}
+
