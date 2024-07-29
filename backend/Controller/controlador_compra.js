580052921b46c0d83c96ad4762b719286f470ce4
LuisPereira-20 <151740701+LuisPereira-20@users.noreply.github.com>
2024-07-29 01:31:04 -0400

    Update controlador_compra.js

---
+++ backend/Controller/controlador_compra.js
@@ -0,0 +1,4 @@
+import compra from "../Model/modelo_compra";
+import usuario from "../Model/modelo_usuario";
+import producto from "../Model/modelo_producto";
+import opciones from "../Tools/opciones";
