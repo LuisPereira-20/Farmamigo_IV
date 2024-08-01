c8af3d1b0595949d5ee746e3099fd662c03cfa26
LuisPereira-20 <151740701+LuisPereira-20@users.noreply.github.com>
2024-08-01 13:41:21 -0400

    inicio

---
+++ frontend/home/src/components/exhibidor.jsx
@@ -0,0 +1,10 @@
+function Exhibicion() {
+    return(
+    <div id="exhibidor"
+    className="bg-blue-900 rounded-xl h-24px w-24px p-4 grid place-items-center my-5" >
+        <p className="text-2xl">Producto</p>
+        <button id="añadir" className=", rounded-xl h-4px w-12px hover:bg-161-161-161">Añadir</button>
+    </div>
+    )
+}
+export default Exhibicion;
